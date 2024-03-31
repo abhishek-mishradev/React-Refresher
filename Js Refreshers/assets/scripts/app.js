0134f1d8b2a9a2ec037915112d2d0b66b27d6e61
Abhishek Mishra <mishra.abhishek01@hotmail.com>
2024-03-31 21:31:35 +0530

    JavaScript Refreshers

--- Js Refreshers/assets/scripts/app.js
+++ Js Refreshers/assets/scripts/app.js
@@ -1,30 +1,33 @@
-// import { apiKey } from "./utils.js";
+// import { apiKey } from "./util.js";
 
-// import  apiKey  from "./utils.js"
+// import apiKey from "./util.js";
+// import { apiKey, abc as content } from "./util.js";
+// import * as util from "./util.js";
 
-// import { apiKey,abc } from "./utils.js";
-// import { apiKey,abc as api2 } from "./utils.js";
-// import *  as utils from './utils.js';
+// console.log(util.apiKey);
+// console.log(content);
 
-// console.log(apiKey, api2);
+// const userMessage = "Hello World!!!";
 
-// console.log(utils.abc);
-// console.log(utils.apiKey);
+// console.log(userMessage);
+// console.log(userMessage);
 
-
-// function greetUser(userName, msg="hello"){
-//     console.log(userName, msg);
+// function createGreeting(userName, message = "Hello!") {
+// console.log(userName);
+// console.log(message);
+//   return "Hi, I am " + userName + ". " + message;
 // }
 
-// greetUser("abhi", "world");
+// const greeting1 = createGreeting("Max");
+// console.log(greeting1);
 
+// const greeting2 = createGreeting("Manuel", "Hello, what's up?");
+// console.log(greeting2);
 
-// function greetUser(userName, msg="hello"){
-//     console.log(userName, msg);
-// }
-
-// const funuser= greetUser("abhi", "world");
-// funuser();
+// export default (userName, message) => {
+//  console.log("Hello");
+//   return userName + message;
+// };
 
 // const user = {
 //   name: "Max",
@@ -53,44 +56,101 @@
 // console.log(user1);
 // user1.greet();
 
-const hobbies = ["Sports", "Cooking", "Reading"];
-console.log(hobbies[0]);
+// const hobbies = ["Sports", "Cooking", "Reading"];
+// console.log(hobbies[0]);
 
+// hobbies.push("Working");
+// console.log(hobbies);
+
+// const index = hobbies.findIndex((item) => item === "Sports");
+
+// console.log(index);
+
+// const editedHobbies = hobbies.map((item) => ({ text: item }));
+// console.log(editedHobbies);
+
+// const [firstName, lastName] = ["Max", "Schwarzmüller"];
+
+// const firstName = userNameData[0];
+// const lastName = userNameData[1];
+
+// console.log(firstName);
+// console.log(lastName);
+
+// const { name: userName, age } = {
+//   name: "Max",
+//   age: 34
+// };
+
+// console.log(userName);
+// console.log(age);
+
+// const name = user.name;
+// const age = user.age;
+
+// const hobbies = ["Sports", "Cooking"];
+// const user = {
+//   name: "Max",
+//   age: 34
+// };
+
+// const newHobbies = ["Reading"];
+
+// const mergedHobbies = [...hobbies, ...newHobbies];
+// console.log(mergedHobbies);
+
+// const extendedUser = {
+//   isAdmin: true,
+//   ...user
+// };
+// console.log(extendedUser);
+
+// const password = prompt("Your password");
+
+// if (password === "Hello") {
+//   console.log("Hello works");
+// } else if (password === "hello") {
+//   console.log("hello works");
+// } else {
+//   console.log("Access not granted.");
+// }
+
+// const hobbies = ["Sports", "Cooking"];
+
+// for (const hobby of hobbies) {
+//   console.log(hobby);
+// }
+
+// const list = document.querySelector("ul");
+// list.remove();
+
+// function handleTimeout() {
+//   console.log("Timed out!");
+// }
+
+// const handleTimeout2 = () => {
+//   console.log("Timed out ... again!");
+// };
+
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => {
+//   console.log("More timing out...");
+// }, 4000);
+
+// function init() {
+//   function greet() {
+//     console.log(“Hi!“);
+//   }
+
+//   greet();
+// }
+
+// init();
+
+// const message = “Hello”;
+
+const hobbies = ["Sports", "Cooking"];
+// hobbies = [];
 hobbies.push("Working");
 console.log(hobbies);
-
-
-const index = hobbies.findIndex((item) => item === "Sports");
-
-console.log(index);
-
-const editedHobbies = hobbies.map((item) => ({ text: item }));
-console.log(editedHobbies);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
